95151d2d24e41631f102d352812ceecb50d808f9
h2cone <tw8ape@gmail.com>
2025-09-07 00:08:34 +0800

    Refactor level and player scenes, add gravity to player, and implement level loading in world

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,5 +1,5 @@
 use godot::{
-    classes::{CharacterBody2D, ICharacterBody2D},
+    classes::{CharacterBody2D, ICharacterBody2D, ProjectSettings},
     prelude::*,
 };
 
@@ -7,19 +7,27 @@ use godot::{
 #[class(base=CharacterBody2D)]
 pub struct Player {
     base: Base<CharacterBody2D>,
+    gravity: f64,
 }
 
 #[godot_api]
 impl ICharacterBody2D for Player {
     fn init(base: Base<CharacterBody2D>) -> Self {
-        Self { base }
+        Self { base, gravity: 0. }
     }
 
     fn ready(&mut self) {
+        let settings = ProjectSettings::singleton();
+        self.gravity = settings.get("physics/2d/default_gravity").to::<f64>();
         godot_print!("Player ready")
     }
 
-    fn physics_process(&mut self, _delta: f64) {}
+    fn physics_process(&mut self, delta: f64) {
+        let mut velocity = self.base().get_velocity();
+        velocity.y += self.gravity as f32 * delta as f32;
+        self.base_mut().set_velocity(velocity);
+        self.base_mut().move_and_slide();
+    }
 }
 
 impl Player {}
--- rust/src/world.rs
+++ rust/src/world.rs
@@ -1,18 +1,33 @@
 use godot::prelude::*;
 
+const LEVELS_LEN: usize = 3;
+
 #[derive(GodotClass)]
 #[class(base=Node2D)]
 pub struct World {
     base: Base<Node2D>,
+    levels: [Option<Gd<PackedScene>>; LEVELS_LEN],
 }
 
 #[godot_api]
 impl INode2D for World {
     fn init(base: Base<Node2D>) -> Self {
-        Self { base }
+        Self {
+            base,
+            levels: std::array::from_fn(|_| None),
+        }
     }
 
     fn ready(&mut self) {
+        for i in 1..LEVELS_LEN {
+            let path = format!("res://level_{}.tscn", i);
+            self.levels[i] = Some(try_load::<PackedScene>(&path).expect("failed to load scene"));
+        }
+        let level_no = 1;
+        if let Some(scene) = &self.levels[level_no] {
+            let new_level = scene.instantiate().expect("failed to instantiate");
+            self.base_mut().add_child(&new_level);
+        }
         godot_print!("World ready")
     }
 }
